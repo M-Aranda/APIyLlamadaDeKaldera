87386f6c6b2da92aeb4a2d540206359224240fcd
Marcelo <cheloz_20@hotmail.com>
2023-08-03 14:38:00 -0400

    Se agregan projectos

---
+++ APIOkKaldera/Controllers/MessageController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace APIOkKaldera.Controllers
+{
+    public class MessageController : ApiController
+    {
+
+        [HttpPost]
+        public IHttpActionResult Post()
+        {
+            return Ok("Este es un mensaje de prueba");
+        }
+
+    }
+}
---
+++ LlamarAPIKaldera/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+class LlamarAPIKaldera
+{
+    static void Main()
+    {
+        var apiUrl = GetApiUrlFromConfiguration(); 
+
+        var timer = new Timer(async _ =>
+        {
+           await CallApiAsync(apiUrl);
+        }, null, TimeSpan.Zero, TimeSpan.FromMinutes(1));
+
+        
+        Console.ReadKey();
+        timer.Dispose();
+    }
+
+    private static string GetApiUrlFromConfiguration()
+    {
+        return System.Configuration.ConfigurationManager.AppSettings["ApiUrl"];
+    }
+
+
+    private static async Task CallApiAsync(string apiUrl)
+    {
+        try
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.PostAsync(apiUrl, null);
+                response.EnsureSuccessStatusCode();
+                Console.WriteLine($"Llamada exitosa: {response.StatusCode}");
+                Console.WriteLine($"Contentido de mensaje: {await response.Content.ReadAsStringAsync()}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
+
+
